f069e07d68a398059cd700ba0a241ca843f65318
Jon Wexler <jwexman@gmail.com>
2019-04-12 02:08:04 -0400

    updates personal site

--- js/main.js
+++ js/main.js
@@ -2,33 +2,35 @@ $(document).ready( function() {
 
 	// Logo
 	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
-        }
-    }
-    
+  if (location.href.indexOf("#") != -1) {
+    if(location.href.substr(location.href.indexOf("#"))!='#about'){
+       $logo.show();
+   }
+}
+
 	// Show logo 
 	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
-	});
+     $logo.slideDown('slow');
+ });
+
 	// Hide logo
 	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
-function animMeter(){
-    $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
+     $logo.slideUp('slow');
+ });
+ 	
+    function animMeter(){
+        $(".meter > span").each(function() {
+            $(this)
+            .data("origWidth", $(this).width())
+            .width(0)
+            .animate({
+                width: $(this).data("origWidth")
+            }, 1200);
+        });
+    }
+    animMeter();
 
-      $('#tab-container').easytabs({
+    $('#tab-container').easytabs({
         animate			: true,
         updateHash		: true,
         transitionIn	: 'slideDown',
@@ -36,7 +38,7 @@ animMeter();
         animationSpeed	: 600,
         tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
             if($targetPanel.selector=='#resume'){
-                    animMeter();
+                animMeter();
             }
         });
     });
